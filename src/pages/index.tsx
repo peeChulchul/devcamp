cee3bea758a1e1a0a4bdece31f209010a7a0dc25
peeChulchul <eyeless356@naver.com>
2024-03-20 11:50:41 +0900

    [fix] 분기처리방식 변경
    로그인 회원가입 컴포넌트에서 context값을 통해 null을 리턴하도록 하였으나 성능상 안좋을 것 같아서(if문 위로 자바스크립트 다운로드 받는 문제) 상위컴포넌트에서 authState의 값에 따라 조건부 렌더링하도록 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,20 @@
+import { Card } from "@/components/ui/card";
+import { cn } from "@/lib/utils";
 import { createContext, useState, useContext, PropsWithChildren } from "react";
+import Login from "./Login";
+import SignIn from "./SignIn";
 
-type TAuthContext = "signIn" | "logIn" | "default";
+type TAuthContext = "signIn" | "logIn";
 type TUpdateContext = (newState: TAuthContext) => void;
 
-const AuthContext = createContext<TAuthContext>("default");
+const AuthContext = createContext<TAuthContext>("signIn");
 const UpdateAuthContext = createContext<TUpdateContext>(() => {});
 
 export const useAuthContext = () => useContext(AuthContext);
 export const useAuthContextUpdate = () => useContext(UpdateAuthContext);
 
 export default function Home() {
-  const [authState, setAuthState] = useState<TAuthContext>("default");
+  const [authState, setAuthState] = useState<TAuthContext>("signIn");
 
   const updateContext = (newState: TAuthContext) => {
     setAuthState(newState);
@@ -19,7 +23,9 @@ export default function Home() {
   return (
     <main>
       <AuthContext.Provider value={authState}>
-        <UpdateAuthContext.Provider value={updateContext}></UpdateAuthContext.Provider>
+        <UpdateAuthContext.Provider value={updateContext}>
+          <Card className={cn("w-[380px] mx-auto")}>{authState === "signIn" ? <SignIn /> : <Login />}</Card>
+        </UpdateAuthContext.Provider>
       </AuthContext.Provider>
     </main>
   );
