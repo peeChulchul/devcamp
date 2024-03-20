3c5eff1d384d58d355ec01a24967f746ae16b31c
peeChulchul <eyeless356@naver.com>
2024-03-20 11:48:25 +0900

    [feat] 데브캠프 타이틀추가

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,18 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { Inter } from "next/font/google";
 
 const inter = Inter({ subsets: ["latin"] });
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div className={inter.className}>
-      <Component {...pageProps} />;
-    </div>
+    <>
+      <Head>
+        <title>데브캠프</title>
+      </Head>
+      <div className={inter.className}>
+        <Component {...pageProps} />
+      </div>
+    </>
   );
 }
