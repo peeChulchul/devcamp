4594e0278fa770e509f9f5a545b7ef4ea7b059f1
peeChulchul <eyeless356@naver.com>
2024-03-20 11:51:06 +0900

    [feat] 로그인 페이지 생성

---
+++ src/pages/Login.tsx
@@ -0,0 +1,14 @@
+import { CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
+import React from "react";
+
+const Login = () => {
+  return (
+    <>
+      <CardHeader>
+        <CardTitle>로그인</CardTitle>
+      </CardHeader>
+    </>
+  );
+};
+
+export default Login;
